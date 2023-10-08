f494a6431473d48dd29534dfb49b6e5fd6cf9a67
thiago gonzalez castello <73041924+thiagogcastello@users.noreply.github.com>
2023-10-08 14:38:55 -0300

    Add files via upload

---
+++ Gonzalez.Castello.Thiago.Parcial/Vista/Program.cs
@@ -0,0 +1,20 @@
+﻿using Entidades;
+
+namespace Vista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creo supervisor
+            Supervisor supervisor1 = new Supervisor("Thiago Gonzalez Castello", 42343951, TipoEmpleado.Supervisor);
+
+            //creo operario
+            Operario operario1 = new Operario("Bart Simpson", 37656991, TipoEmpleado.Operario);
+
+            Console.WriteLine(supervisor1.MostrarInfo());
+            Console.WriteLine(operario1.MostrarInfo());
+
+        }
+    }
+}
