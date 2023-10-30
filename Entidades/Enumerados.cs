394d6727dcaa8eed982409e2cde656799cde66c0
thiago gonzalez castello <73041924+thiagogcastello@users.noreply.github.com>
2023-10-29 21:41:21 -0300

    Delete Entidades directory

--- Entidades/Enumerados.cs
+++
@@ -1,8 +0,0 @@
-﻿public enum TipoEmpleado {Operario, Supervisor};
-
-public enum MateriasPrimas {pantalones, sombreros, botas, camisas, muñecos,
-                            algodon, tela, ojos, cajas}
-
-public enum ProcesosProduccionWoody {vestir, pintar, empaquetar}
-
-public enum ProcesoBabyYoda {rellenar, coser, aplicarMagia, empaquetar}
