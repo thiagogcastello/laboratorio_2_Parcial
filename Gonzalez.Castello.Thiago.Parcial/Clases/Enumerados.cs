ae2d9e595a03b9619db8aade3d872002a1d6d67b
thiago gonzalez castello <73041924+thiagogcastello@users.noreply.github.com>
2023-10-29 21:41:13 -0300

    Delete Gonzalez.Castello.Thiago.Parcial directory

--- Gonzalez.Castello.Thiago.Parcial/Clases/Enumerados.cs
+++
@@ -1,6 +0,0 @@
-﻿public enum TipoEmpleado {Operario, Supervisor};
-
-public enum EstadoProducto {disponible, sinStock, inactivo }
-
-public enum MateriasPrimas {pantalones, sombreros, botas, camisas, muñecos,
-                            algodon, tela, ojos}
